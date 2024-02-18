ade28bd4435eef4de458199e130901ab1439315e
Ayush Khanduri <113684157+ayushkhanduri74@users.noreply.github.com>
2024-02-18 18:27:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
+  git clone https://github.com/ayushkhanduri74/Piro_EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /Piro_EvaMaria
 fi
-cd /PiroAutoFilterBot
+cd /Piro_EvaMaria
 pip3 install -U -r requirements.txt
-echo "Starting PiroAutoFilterBot...."
+echo "Starting Piro_EvaMaria...."
 python3 bot.py
